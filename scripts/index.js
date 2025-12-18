dcd88e8a2f5fb9f9245129f1f4fa6ba495e07d22
BryanJ <bryan872010@hotmail.com>
2025-12-18 13:22:45 -0600

    Sprint 8

--- scripts/index.js
+++ scripts/index.js
@@ -24,6 +24,47 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
   },
 ];
+
+const profileEditButton = document.querySelector(".profile__edit-button");
+const profileModal = document.getElementById("edit-popup");
+const modalCloseButton = profileModal.querySelector(".popup__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const nameInput = profileModal.querySelector(".popup__input_type_name");
+const descriptionInput = profileModal.querySelector(
+  ".popup__input_type_description"
+);
+const profileForm = profileModal.querySelector("#edit-profile-form");
+
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+function fillProfileForm() {
+  nameInput.value = profileTitle.textContent;
+  descriptionInput.value = profileDescription.textContent;
+}
+
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal(profileModal);
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+  closeModal(profileModal);
+}
+
+profileEditButton.addEventListener("click", handleOpenEditModal);
+modalCloseButton.addEventListener("click", () => closeModal(profileModal));
+profileForm.addEventListener("submit", handleProfileFormSubmit);
+
 initialCards.forEach((card) => {
   console.log(card.name);
 });
